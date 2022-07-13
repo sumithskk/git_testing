8ad43833174742cc56a72c51c8972f087096495f
K K <sumith.k-k@capgemini.com>
2022-07-13 12:37:20 +0530

    new file added

---
+++ lenth_fun.py
@@ -0,0 +1,9 @@
+# import numpy as np
+def sample_function(ar):
+    if (len(ar)>10):
+        print('Length is greater than 10')
+
+    else:
+        print('Length is less than 10')
+
+sample_function([1,0,2,6,4,7,8,7])
